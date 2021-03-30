47ee5bc3561e9442db630202188693f50af4ab5d
MalbonaGenio <MalbonaGenio@users.noreply.github.com>
2021-03-30 14:57:26 +0900

    added useState Using useState we keep track of the query that the user inputs, and from the response from TMDB we store the data.response in movie to keep its state and render cards for each movie

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -1,23 +1,24 @@
-import React from "react"
+import React, { useState } from "react"
 
-function SearchMovies() {
+export default function SearchMovies() {
+  const [query, setQuery] = useState("")
+  const [movies, setMovies] = useState([])
   const searchMovies = async (e) => {
     e.preventDefault()
-    console.log("submitting")
-    const query = "jurassic"
+
     const url = `https://api.themoviedb.org/3/search/movie?api_key=b2e594d821d4edcbc8068cf5936b83c3&query=${query}&page=1&include_adult=false`
 
     try {
       const res = await fetch(url)
       const data = await res.json()
-      console.log(data)
+      setMovies(data.results)
     } catch (err) {
       console.error(err)
     }
   }
 
   return (
-    <div>
+    <>
       <form className="form" onSubmit={searchMovies}>
         <label htmlFor="query" className="label">
           Movie Name
@@ -27,11 +28,22 @@ function SearchMovies() {
           type="text"
           name="query"
           placeholder="Your search here"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
         />
         <button className="button">Search</button>
       </form>
-    </div>
+      <div className="card-list">
+        {movies.map((movie) => (
+          <div className="card">
+            <img
+              className="card--image"
+              src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+              alt={movie.title + " poster"}
+            />
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
-
-export default SearchMovies
