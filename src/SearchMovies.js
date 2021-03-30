7a2f0243077f780fcf417a659a6257ccb5b079dd
MalbonaGenio <MalbonaGenio@users.noreply.github.com>
2021-03-30 13:59:36 +0900

    added search function added a fucntion to call the api using the key provided and the query, for now the query is hard coded to test the return from the TMDB

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -1,9 +1,24 @@
 import React from "react"
 
 function SearchMovies() {
+  const searchMovies = async (e) => {
+    e.preventDefault()
+    console.log("submitting")
+    const query = "jurassic"
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=b2e594d821d4edcbc8068cf5936b83c3&query=${query}&page=1&include_adult=false`
+
+    try {
+      const res = await fetch(url)
+      const data = await res.json()
+      console.log(data)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
   return (
     <div>
-      <form className="form" onSubmit="##">
+      <form className="form" onSubmit={searchMovies}>
         <label htmlFor="query" className="label">
           Movie Name
         </label>
